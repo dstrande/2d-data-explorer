ee9141435daf5b4f971c0eeb78d34044454f58d5
dstrandell <dstrande@live.ca>
2024-05-07 12:16:04 -0700

    2d dataset and use async

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask
 import json
 from bokeh.plotting import figure
 from bokeh.embed import json_item
+from bokeh.palettes import Inferno256
+from bokeh.models import FixedTicker, ColorBar, LinearColorMapper
 from flask_cors import cross_origin
+import numpy as np
 
 app = Flask(__name__)
 
@@ -10,17 +13,25 @@ app = Flask(__name__)
 @app.route("/plot")
 @cross_origin(origin="localhost")  # Allow only localhost:3000 to access
 def plot():
-    # Create a new plot with a title and axis labels
-    p = figure()  # title="Simple Line Example", x_axis_label="x", y_axis_label="y")
+    x, y = np.meshgrid(np.linspace(0, 3, 40), np.linspace(0, 2, 30))
+    z = 1.3 * np.exp(-2.5 * ((x - 1.3) ** 2 + (y - 0.8) ** 2)) - 1.2 * np.exp(
+        -2 * ((x - 1.8) ** 2 + (y - 1.3) ** 2)
+    )
 
-    # Add a line renderer with legend and line thickness
-    p.line([1, 2, 3], [1, 4, 9], line_width=2)  # , legend_label="Temp."
+    p = figure(width=550, height=300, x_range=(0, 3), y_range=(0, 2))
+    levels = np.linspace(-1, 1, 9)
+    p.contour(x, y, z, levels, fill_color=Inferno256, line_color="black")
+    color_mapper = LinearColorMapper(palette="Inferno256", low=-1, high=1)
+    color_bar = ColorBar(
+        color_mapper=color_mapper,
+        ticker=FixedTicker(ticks=[-1, 0, 1]),
+    )
+    p.add_layout(color_bar, "right")
 
     model = json.dumps(json_item(p, "myplot"))
-    # print(model, flush=True)
 
     # Return the plot as json
-    return model  # jsonify(json_item(p, "myplot"))
+    return model
 
 
 if __name__ == "__main__":
--- react-src/src/App.js
+++ react-src/src/App.js
@@ -2,33 +2,28 @@ import React, { useEffect } from 'react';
 
 function Plot() {
   useEffect(() => {
-      fetch('http://localhost:5000/plot')
-          .then(function(response) { return response.json() })
-          .then(function(item) { return Bokeh.embed.embed_item(item) })
-          .catch(error => console.error('Error loading the plot:', error))
+    console.log("rendered");  
+      async function fetchPlot() {
+          try {
+              const response = await fetch('http://localhost:5000/plot');
+              let item;
+              if (response.headers.get("content-type").includes("application/json")) {
+                  item = await response.json();
+              } else {
+                  const text = await response.text();
+                  item = JSON.parse(text);
+              }
+
+              Bokeh.embed.embed_item(item, "myplot");
+          } catch (error) {
+              console.error('Error loading the plot:', error);
+          }
+      }
+
+      fetchPlot();
   }, []);
 
   return <div id="myplot"></div>;
 }
 
 export default Plot;
-
-
-// import React, { useEffect } from 'react';
-
-// function Plot() {
-//   useEffect(() => {
-//       fetch('http://localhost:5000/plot')
-//           .then(response => response.json())
-//           .then(data => {
-//               console.log(data);
-//               data = JSON.parse(data);
-//               Bokeh.embed.embed_item(data); //, "myplot"
-//           })
-//           .catch(error => console.error('Error loading the plot:', error));
-//   }, []);
-
-//   return <div id="myplot"></div>;
-// }
-
-// export default Plot;
